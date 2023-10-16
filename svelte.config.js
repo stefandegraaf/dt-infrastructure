7bafe75bf4ffa8321eb2ed90af33712c22420967
Stefan de Graaf <stefan.de.graaf@geodan.nl>
2023-10-16 14:24:54 +0200

    github deployment settings

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from "svelte-preprocess";
 import { optimizeImports } from "carbon-preprocess-svelte";
 
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -26,7 +25,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
+			base: process.argv.includes('dev') ? '' : `${process.env.BASE_PATH}/src`,
         }
 	}
 };
