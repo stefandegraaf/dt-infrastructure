5fd14e96ae9f40469cb08f096b8330d1f552e2df
Stefan de Graaf <stefan.de.graaf@geodan.nl>
2023-10-16 12:40:30 +0200

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from "svelte-preprocess";
 import { optimizeImports } from "carbon-preprocess-svelte";
@@ -11,14 +11,20 @@ const config = {
 		preprocess({
 			typescript: true
 		}),
-		optimizeImports({})
+		optimizeImports({}),
+		vitePreprocess()
 	],
+
+	//preprocess: vitePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/dt-infrastructure' : '',
+        }
 	}
 };
 
