66a0b61bfa56c5ce76d60406629bcfd1c2321152
Stefan de Graaf <stefan.de.graaf@geodan.nl>
2024-03-04 16:32:31 +0100

    update import

--- src/lib/components/animation/renders/glb-render.ts
+++ src/lib/components/animation/renders/glb-render.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 import type { RenderHandler } from '../render-handler';
 import { createNoise2D } from 'simplex-noise';
